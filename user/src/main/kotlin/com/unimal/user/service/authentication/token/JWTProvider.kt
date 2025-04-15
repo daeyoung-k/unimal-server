31c8889fe4e176352b2d98e896353408e8e9e762
kkwon_dae <eodud4976@gmail.com>
2025-04-15 22:36:00 +0900

    JWT 액세스, 리프레쉬 토큰 발급, 인증

--- user/src/main/kotlin/com/unimal/user/service/authentication/token/JWTProvider.kt
+++ user/src/main/kotlin/com/unimal/user/service/authentication/token/JWTProvider.kt
@@ -1,28 +1,53 @@
 package com.unimal.user.service.authentication.token
 
-import io.jsonwebtoken.io.Decoders
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.security.Keys
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
+import java.util.*
 
 @Component
 class JWTProvider {
-
     @Value("\${custom.jwt.secret-key}")
     lateinit var secretKey: String
-    private val key by lazy { Keys.hmacShaKeyFor(secretKey.toByteArray()) }
+    private val key by lazy { Keys.hmacShaKeyFor(Base64.getDecoder().decode(secretKey)) }
+    private val accessExpiration: Long = 1000L * 60 * 60 // 1hour
+    private val refreshExpiration: Long = 1000L * 60 * 60 * 24 * 180 // 180days
 
-    fun createAccessToken() {
-        val test: String = this.secretKey + "test"
-        println(test)
-        println(this.key)
+    fun createAccessToken(
+        email: String,
+        role: List<String>
+    ): String {
+        return Jwts.builder()
+            .expiration(Date(System.currentTimeMillis() + accessExpiration))
+            .signWith(key)
+            .claim("type", "access")
+            .claim("role", role)
+            .subject(email)
+            .compact()
     }
 
-    fun createRefreshToken() {
-
+    fun createRefreshToken(
+        email: String,
+        role: List<String>
+    ): String {
+        return Jwts.builder()
+            .expiration(Date(System.currentTimeMillis() + refreshExpiration))
+            .signWith(key)
+            .claim("type", "refresh")
+            .claim("role", role)
+            .subject(email)
+            .compact()
     }
 
-    fun getClaims() {
-
+    fun getClaims(
+        token: String
+    ): Claims {
+        return Jwts.parser()
+            .verifyWith(key)
+            .build()
+            .parseSignedClaims(token)
+            .payload
     }
 }
