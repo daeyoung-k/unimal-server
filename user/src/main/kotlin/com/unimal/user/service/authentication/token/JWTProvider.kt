43feae658a84b72c88e25cfe8c942d44cc3df625
kkwon_dae <eodud4976@gmail.com>
2025-04-15 23:25:02 +0900

    토큰 인증 예외처리 추가, 예외 핸들러 수정, 로그 추가 적용

--- user/src/main/kotlin/com/unimal/user/service/authentication/token/JWTProvider.kt
+++ user/src/main/kotlin/com/unimal/user/service/authentication/token/JWTProvider.kt
@@ -1,14 +1,23 @@
 package com.unimal.user.service.authentication.token
 
+import com.unimal.user.exception.CustomException
+import io.github.oshai.kotlinlogging.KotlinLogging
 import io.jsonwebtoken.Claims
+import io.jsonwebtoken.ExpiredJwtException
 import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.MalformedJwtException
 import io.jsonwebtoken.security.Keys
+import io.jsonwebtoken.security.SignatureException
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 import java.util.*
 
 @Component
 class JWTProvider {
+
+    val logger = KotlinLogging.logger {}
+
     @Value("\${custom.jwt.secret-key}")
     lateinit var secretKey: String
     private val key by lazy { Keys.hmacShaKeyFor(Base64.getDecoder().decode(secretKey)) }
@@ -44,10 +53,21 @@ class JWTProvider {
     fun getClaims(
         token: String
     ): Claims {
-        return Jwts.parser()
-            .verifyWith(key)
-            .build()
-            .parseSignedClaims(token)
-            .payload
+        try {
+            return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .payload
+        } catch (e: ExpiredJwtException) {
+            throw e
+        } catch (e: SignatureException) {
+            throw e
+        } catch (e: MalformedJwtException) {
+            throw e
+        } catch (e: Exception) {
+            logger.error { "토큰 인증 실패: ${e.message}" }
+            throw CustomException("토큰 인증 실패", 401, HttpStatus.UNAUTHORIZED)
+        }
     }
 }
