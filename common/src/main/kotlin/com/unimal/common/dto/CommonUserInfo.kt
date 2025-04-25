785916cc6c3d16d405d6fc5b250f853d129f0d19
daeyoung-k <eodud4976@gmail.com>
2025-04-25 16:29:48 +0900

    유저 정보 헤더전달 완료, role 정보 dto list string 변환 적용

--- common/src/main/kotlin/com/unimal/common/dto/CommonUserInfo.kt
+++ common/src/main/kotlin/com/unimal/common/dto/CommonUserInfo.kt
@@ -1,7 +1,10 @@
 package com.unimal.common.dto
 
-data class UserInfo(
+data class CommonUserInfo(
     val email: String,
     val roles: List<String>,
     val provider: String,
-)
+) {
+    val roleString: String
+        get() = roles.joinToString(",")
+}
