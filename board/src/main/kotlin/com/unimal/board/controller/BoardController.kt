34b667d359b8ed2b606fc605d791117cb8a0dc05
kkwon_dae <eodud4976@gmail.com>
2025-06-26 23:28:17 +0900

    게시판 생성 컨트롤러 생성 (multipart)

--- board/src/main/kotlin/com/unimal/board/controller/BoardController.kt
+++ board/src/main/kotlin/com/unimal/board/controller/BoardController.kt
@@ -1,12 +1,17 @@
 package com.unimal.board.controller
 
+import com.unimal.board.controller.request.ImageMetadata
+import com.unimal.board.controller.request.PostsCreateRequest
+import com.unimal.board.service.BoardService
 import com.unimal.common.annotation.user.UserInfoAnnotation
 import com.unimal.common.dto.CommonResponse
 import com.unimal.common.dto.CommonUserInfo
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.multipart.MultipartFile
 
 @RestController
 class BoardController(
+    private val boardService: BoardService
 ) {
 
     @GetMapping("/posts/list")
@@ -14,11 +19,14 @@ class BoardController(
         return CommonResponse(data = "게시글 목록 조회")
     }
 
-    @PostMapping("/posts/create")
+    @PostMapping("/posts/create", consumes = ["multipart/form-data"])
     fun createPost(
-         @UserInfoAnnotation userInfo: CommonUserInfo
-        // postsCreateRequest: PostsCreateRequest,
+        @UserInfoAnnotation userInfo: CommonUserInfo,
+        @ModelAttribute postsCreateRequest: PostsCreateRequest,
+        @RequestPart("imageMetadata") imageMetadata: List<ImageMetadata>?,
+        @RequestPart("images", required = false) images: List<MultipartFile>?,
     ): CommonResponse {
+        boardService.createPost(userInfo, postsCreateRequest, imageMetadata, images)
         return CommonResponse(data = "게시글 생성")
     }
 
--- board/src/main/kotlin/com/unimal/board/service/BoardService.kt
+++ board/src/main/kotlin/com/unimal/board/service/BoardService.kt
@@ -1,11 +1,22 @@
 package com.unimal.board.service
 
+import com.unimal.board.controller.request.ImageMetadata
+import com.unimal.board.controller.request.PostsCreateRequest
+import com.unimal.common.dto.CommonUserInfo
 import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
 
 @Service
 class BoardService {
 
-    fun testService(): String {
-        return "Hello, Board Service!"
+    fun createPost(
+        userInfo: CommonUserInfo,
+        postsCreateRequest: PostsCreateRequest,
+        imageMetadata: List<ImageMetadata>?,
+        images: List<MultipartFile>?
+    ) {
+
     }
+
+
 }
