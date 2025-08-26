728e62a2e3baa727555e2e6a86672d3198298258
daeyoung-k <eodud4976@gmail.com>
2025-08-26 16:57:25 +0900

    포토모듈 s3 작업 ing

--- photo/src/main/kotlin/com/unimal/photo/controller/PhotoController.kt
+++ photo/src/main/kotlin/com/unimal/photo/controller/PhotoController.kt
@@ -1,14 +1,9 @@
 package com.unimal.photo.controller
 
-import com.unimal.common.TestDTO
 import com.unimal.common.dto.CommonResponse
 import com.unimal.photo.service.S3Service
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
-import software.amazon.awssdk.services.s3.model.PutObjectRequest
 
 @RestController
 class PhotoController(
@@ -19,7 +14,13 @@ class PhotoController(
     fun upload(
         @RequestBody file: MultipartFile
     ): CommonResponse {
-        s3Service.uploadFile(file)
-        return CommonResponse()
+        return CommonResponse(data = s3Service.uploadFile(file))
+    }
+
+    @GetMapping("/file-url")
+    fun getFileUrl(
+        @RequestParam(value = "key", required = true) key: String,
+    ): CommonResponse {
+        return CommonResponse(data = s3Service.getFileUrl(key))
     }
 }
--- photo/src/main/kotlin/com/unimal/photo/service/S3Service.kt
+++ photo/src/main/kotlin/com/unimal/photo/service/S3Service.kt
@@ -1,27 +1,19 @@
 package com.unimal.photo.service
 
-import org.springframework.beans.factory.annotation.Value
+import com.unimal.photo.service.s3.S3Manager
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
-import software.amazon.awssdk.core.sync.RequestBody
-import software.amazon.awssdk.services.s3.S3Client
-import software.amazon.awssdk.services.s3.model.PutObjectRequest
 
 @Service
 class S3Service(
-    @Value("\${aws.s3.bucket}")
-    private val s3Bucket: String,
-    private val s3Client: S3Client,
+    private val s3Manager: S3Manager
 ) {
 
-    fun uploadFile(file: MultipartFile) {
+    fun uploadFile(file: MultipartFile): String {
+        return s3Manager.imageUpload(file)
+    }
 
-        val fileReq = PutObjectRequest.builder()
-            .bucket(s3Bucket)
-            .key("images/${file.originalFilename}")
-            .build()
-
-        s3Client.putObject(fileReq, RequestBody.fromInputStream(file.inputStream, file.size))
-
+    fun getFileUrl(key: String): String {
+        return s3Manager.getPresignedUrl(key)
     }
 }
