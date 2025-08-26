c48152e4322f6b84ddb05b077ebe6e242ae41777
kkwon_dae <eodud4976@gmail.com>
2025-08-27 00:58:28 +0900

    Update S3Manager.kt

--- photo/src/main/kotlin/com/unimal/photo/service/s3/S3Manager.kt
+++ photo/src/main/kotlin/com/unimal/photo/service/s3/S3Manager.kt
@@ -6,6 +6,8 @@ import org.springframework.web.multipart.MultipartFile
 import software.amazon.awssdk.core.sync.RequestBody
 import software.amazon.awssdk.services.s3.S3Client
 import software.amazon.awssdk.services.s3.model.GetObjectRequest
+import software.amazon.awssdk.services.s3.model.HeadObjectRequest
+import software.amazon.awssdk.services.s3.model.NoSuchKeyException
 import software.amazon.awssdk.services.s3.model.PutObjectRequest
 import software.amazon.awssdk.services.s3.presigner.S3Presigner
 import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest
@@ -23,18 +25,38 @@ class S3Manager(
         file: MultipartFile
     ): String {
         val path = "images/"
-        val key = path + file.originalFilename
+        val originalFilename = file.originalFilename ?: "unnamed"
+        val key = path + originalFilename
+        println("Uploading file with key: $key")
+        
         val fileReq = PutObjectRequest.builder()
             .bucket(s3Bucket)
             .key(key)
+            .contentType(file.contentType)
             .build()
         s3Client.putObject(fileReq, RequestBody.fromInputStream(file.inputStream, file.size))
+        println("Successfully uploaded file with key: $key")
         return key
     }
 
     fun getPresignedUrl(
         key: String
     ): String {
+        println("Requesting presigned URL for key: $key")
+        
+        // 키 존재 여부 확인
+        try {
+            val headObject = HeadObjectRequest.builder()
+                .bucket(s3Bucket)
+                .key(key)
+                .build()
+            s3Client.headObject(headObject)
+            println("Object exists for key: $key")
+        } catch (e: NoSuchKeyException) {
+            println("Object does not exist for key: $key")
+            throw IllegalArgumentException("File not found with key: $key", e)
+        }
+
         val getObject = GetObjectRequest.builder()
             .bucket(s3Bucket)
             .key(key)
@@ -45,7 +67,8 @@ class S3Manager(
             .getObjectRequest(getObject)
             .build()
 
-        return s3Presigner.presignGetObject(presignedObject).url().toString()
+        val result = s3Presigner.presignGetObject(presignedObject)
+        return result.url().toString()
     }
 
 }
