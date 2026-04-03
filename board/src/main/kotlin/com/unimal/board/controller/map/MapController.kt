4930993f69e421c5666d8acf3e43f7ab1971fb7c
daeyoung-k <eodud4976@gmail.com>
2026-04-03 17:42:10 +0900

    feat: 지도 기반 게시물 조회 API 구현
    - ZoomLevel enum 추가 (줌 10~20, 반경/게시물 수 정의)
    - MapBoardRepositoryImpl: PostGIS ST_DWithin + 복합 점수 정렬 네이티브 쿼리 구현
      (본인글 우선, 신선도 감쇠, 좋아요/댓글 가중치)
    - LATERAL JOIN으로 대표 이미지 1장 조회, 사진 없는 게시물 제외
    - ST_Y/ST_X로 위도·경도 추출하여 MapPostInfo DTO로 반환
    - MapController, MapPostService, LocationPostRequest 추가

--- board/src/main/kotlin/com/unimal/board/controller/map/MapController.kt
+++ board/src/main/kotlin/com/unimal/board/controller/map/MapController.kt
@@ -1,7 +1,27 @@
 package com.unimal.board.controller.map
 
+import com.unimal.board.controller.map.dto.LocationPostRequest
+import com.unimal.board.service.post.MapPostService
+import com.unimal.common.annotation.user.UserInfoAnnotation
+import com.unimal.common.dto.CommonResponse
+import com.unimal.common.dto.CommonUserInfo
+import jakarta.validation.Valid
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class MapController {
+@RequestMapping("/map")
+class MapController(
+    private val mapPostService: MapPostService
+) {
+
+    @GetMapping("/location/post")
+    fun locationPostList(
+        @UserInfoAnnotation userInfo: CommonUserInfo,
+        @ModelAttribute @Valid locationPostRequest: LocationPostRequest
+    ): CommonResponse {
+        return CommonResponse(data = mapPostService.getLocationPosts(userInfo, locationPostRequest))
+    }
 }
